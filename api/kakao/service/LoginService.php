bd06b4a5bcaf4f06cd0b4207f429cc6b01cb08b2
mcpeleeGit <mcpelee@gmail.com>
2020-12-05 11:59:43 +0900

    upload

--- api/kakao/service/LoginService.php
+++ api/kakao/service/LoginService.php
@@ -49,6 +49,7 @@ class LoginService extends service {
         $userId = $profileObj->id;
         $userName = $profileObj->properties->nickname;
         $userEmail = $profileObj->kakao_account->email;
+        
         echo '<meta charset="utf-8">';
         echo '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">';
         echo '<h1>Kakao Login CallBack</h1>';
