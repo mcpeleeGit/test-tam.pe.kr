db2716af5ee3ab6a896abe6c766dafc99dc17c2e
mcpeleeGit <mcpelee@gmail.com>
2020-12-16 18:39:58 +0900

    upload

--- kakao_talk_message.php
+++ kakao_talk_message.php
@@ -126,7 +126,7 @@ $KakaoAPIService = new KakaoAPIService();
         Kakao.API.request({
             url: '/v1/api/talk/friends/message/default/send',
             data: {
-                receiver_uuids: ['rZysmaufrZStgbiOuY66jryQo5OmlKCY-A'],
+                receiver_uuids: ['rZysnKWWpp6ni7KEvIy4jLuXpJShk6ef_A'],
                 template_object: {
                     object_type: 'feed',
                     content: {
@@ -197,7 +197,6 @@ $KakaoAPIService = new KakaoAPIService();
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script>
-    alert(document.getElementById("JavaScript").innerHTML);
         document.getElementById("sample").innerHTML = document.getElementById("JavaScript").innerHTML.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
         hljs.initHighlightingOnLoad();
     </script>
