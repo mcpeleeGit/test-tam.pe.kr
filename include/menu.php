e72d64f4f4087319d8311735b11e6820d389f71f
mcpeleeGit <mcpelee@gmail.com>
2025-03-15 18:42:00 +0900

    update

--- include/menu.php
+++ include/menu.php
@@ -4,6 +4,5 @@
                 <a class="p-2 link-secondary" href="/js_sdk_v2.php">JS SDK v2</a>
                 <a class="p-2 link-secondary" href="/kakao_talk_message.html">카카오톡 공유</a>
                 <a class="p-2 link-secondary" href="/kakao_talk_social.php">카카오톡 소셜</a>
-                <a class="p-2 link-secondary" href="/kakao_talk_message.php">카카오톡 메시지</a>
             </nav>
         </div>
