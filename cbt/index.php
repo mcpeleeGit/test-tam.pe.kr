3f9ae9b67e50f47d3faebfa5d89f2fbdc3b52c12
동하 <mcpelee@gmail.com>
2025-07-22 16:50:47 +0900

    Add files via upload

---
+++ cbt/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CBT 이미지 테이블</title>
+    <style>
+
+    </style>
+</head>
+<body>
+    <h1 style="text-align:center">CBT 이미지 테이블</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>mac</th>
+                <th>ios</th>
+                <th>win</th>
+                <th>and</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td><img src="cbt1.png" alt="cbt1.png" width="455px"></td>
+                <td><img src="cbt2.jpeg" alt="cbt2.jpeg" width="401px" ></td>
+                <td><img src="cbt3.png" alt="cbt3.png" width="436px"></td>
+                <td><img src="cbt4.png" alt="cbt4.png" width="300px"></td>
+            </tr>
+        </tbody>
+    </table>
+    <table>
+        <thead>
+            <tr>
+                <th>mac</th>
+                <th>ios</th>
+                <th>win</th>
+                <th>and</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td><img src="cbt11.png" alt="cbt11.png" width="455px"></td>
+                <td><img src="cbt21.jpeg" alt="cbt21.jpeg" width="401px" ></td>
+                <td><img src="cbt31.png" alt="cbt31.png" width="436px"></td>
+                <td><img src="cbt4.png" alt="cbt4.png" width="300px"></td>
+            </tr>
+        </tbody>
+    </table>    
+    <table>
+        <thead>
+            <tr>
+                <th>mac</th>
+                <th>ios</th>
+                <th>win</th>
+                <th>and</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td><img src="cbt121.png" alt="cbt121.png" width="430px"><br/>
+                <img src="cbt122.png" alt="cbt122.png" width="430px"><br/>
+                <img src="cbt123.png" alt="cbt123.png" width="430px"></td>
+                <td valign="top"><img src="cbt221.jpeg" alt="cbt221.jpeg" width="401px" ></td>
+                <td><img src="cbt321.png" alt="cbt321.png" width="370px"><br/>
+                <img src="cbt322.png" alt="cbt321.png" width="370px"></td>
+                <td><img src="cbt421.png" alt="cbt421.png" width="300px"></td>
+            </tr>
+        </tbody>
+    </table>        
+</body>
+</html>
