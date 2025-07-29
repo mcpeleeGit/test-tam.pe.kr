b29074389096da2201c61f8fe3207f499b835baf
mcpeleeGit <mcpelee@gmail.com>
2025-07-29 17:17:43 +0900

    Add kakao share

--- cbt/index.php
+++ cbt/index.php
@@ -24,7 +24,10 @@
                 <td><img src="cbt1.png" alt="cbt1.png" width="455px"></td>
                 <td><img src="cbt2.jpeg" alt="cbt2.jpeg" width="401px" ></td>
                 <td><img src="cbt3.png" alt="cbt3.png" width="436px"></td>
-                <td><img src="cbt4.png" alt="cbt4.png" width="300px"></td>
+                <td><img src="cbt41.jpeg" alt="cbt41.jpeg" width="350px"><br/>
+                <img src="cbt42.jpeg" alt="cbt42.jpeg" width="350px"><br/>
+                <img src="cbt43.jpeg" alt="cbt43.jpeg" width="350px">
+            </td>
             </tr>
         </tbody>
     </table>
@@ -41,8 +44,9 @@
             <tr>
                 <td><img src="cbt11.png" alt="cbt11.png" width="455px"></td>
                 <td><img src="cbt21.jpeg" alt="cbt21.jpeg" width="401px" ></td>
-                <td><img src="cbt31.png" alt="cbt31.png" width="436px"></td>
-                <td><img src="cbt4.png" alt="cbt4.png" width="300px"></td>
+                <td><img src="cbt31.png" alt="cbt31.png" width="440px"></td>
+                <td><img src="cbt411.jpeg" alt="cbt411.jpeg" width="370px"><br/>
+                <img src="cbt412.jpeg" alt="cbt412.jpeg" width="370px"></td>
             </tr>
         </tbody>
     </table>    
@@ -61,11 +65,33 @@
                 <img src="cbt122.png" alt="cbt122.png" width="430px"><br/>
                 <img src="cbt123.png" alt="cbt123.png" width="430px"></td>
                 <td valign="top"><img src="cbt221.jpeg" alt="cbt221.jpeg" width="401px" ></td>
-                <td><img src="cbt321.png" alt="cbt321.png" width="370px"><br/>
-                <img src="cbt322.png" alt="cbt321.png" width="370px"></td>
-                <td><img src="cbt421.png" alt="cbt421.png" width="300px"></td>
+                <td><img src="cbt321.png" alt="cbt321.png" width="375px"><br/>
+                <img src="cbt322.png" alt="cbt321.png" width="375px"></td>
+                <td><img src="cbt421.jpeg" alt="cbt421.jpeg" width="313px"><br/>
+                <img src="cbt422.jpeg" alt="cbt422.jpeg" width="313px"><br/>
+                <img src="cbt423.jpeg" alt="cbt423.jpeg" width="313px"><br/>
+                <img src="cbt423.jpeg" alt="cbt423.jpeg" width="313px"></td>
             </tr>
         </tbody>
-    </table>        
+    </table>     
+    <table>
+        <thead>
+            <tr>
+                <th>mac</th>
+                <th>ios</th>
+                <th>win</th>
+                <th>and</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td><img src="cbt131.png" alt="cbt131.png" width="455px"></td>
+                <td valign="top"><img src="cbt231.jpeg" alt="cbt231.jpeg" width="401px" ></td>
+                <td valign="top"><img src="cbt331.png" alt="cbt331.png" width="460px"></td>
+                <td valign="top"><img src="cbt431.jpeg" alt="cbt431.jpeg" width="380px"></td>
+            </tr>
+        </tbody>
+    </table>      
+    
 </body>
 </html>
