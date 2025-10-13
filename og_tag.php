596640d410d7f1f9d37a366c2a145d32a17d8ba1
mcpeleeGit <mcpelee@gmail.com>
2025-10-13 18:54:40 +0900

    og tag test

--- og_tag.php
+++ og_tag.php
@@ -8,7 +8,7 @@
     <meta property="og:type" content="website">
     <meta property="og:title" content="2025 제2회 세금 재테크 포럼 | 한국벤처경영원">
     <meta property="og:description" content="미국 국채·스테이블코인·부동산 자산·음악저작권 | 실물 자산 투자와 연결된 소득공제">
-    <meta property="og:image" content="https://www.kofund.co.kr/event/img/forum2025/link_thumbnail_v2.png">
+    <meta property="og:image" content="http://test-tam.pe.kr/images/link_thumbnail_v2.png">
     <meta property="og:image:width" content="1200">
     <meta property="og:image:height" content="630">
     <meta property="og:image:type" content="image/png">
