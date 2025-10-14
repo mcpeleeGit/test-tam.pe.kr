14145ffbf5c0bcac3830a5833efea6e0e37758c0
mcpeleeGit <mcpelee@gmail.com>
2025-10-14 14:22:51 +0900

    og tag test

--- og_tag.php
+++ og_tag.php
@@ -9,7 +9,7 @@
     <meta property="og:title" content="배민상회 - 배달사장님의 비용을 아껴주는 식자재 장보기">
     <meta property="og:type" content="website">
     <meta property="og:description" content="신선 식재료, 포장용기, 배달비품 전문 쇼핑몰, 배민상회입니다. 톡톡튀는 배민 디자인 제품과 품질로 엄선한 기성품을 특별 할인가로 만나보세요."><meta property="og:url" content="https://mart.baemin.com">
-    <meta property="og:image" content="https://cdn-mart.baemin.com/front-end/assets-static/open_graph_2021.png">  
+    <meta property="og:image" content="https://cdn-mart.baemin.com/front-end/assets-static/open_graph_2021.png?1=1">  
     <link rel="shortcut icon" href="https://cdn-mart.baemin.com/front-end/assets-static/favicon_2021.ico">
     <link rel="icon" href="https://cdn-mart.baemin.com/front-end/assets-static/favicon_2021.ico">
     <link rel="apple-touch-icon" href="https://cdn-mart.baemin.com/front-end/assets-static/touch_icon_2021.png">
